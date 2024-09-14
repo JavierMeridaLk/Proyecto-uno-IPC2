9fd3e9ac54787fa5266b589010d8d8be402dc28e
Javier Mérida <javiermerida202230638@cunoc.edu.gt>
2024-09-14 03:14:25 -0600

    verificacion de usuario

--- proyectoUnoIPC/src/main/java/backen/serverts/inicioDeSesionSvt.java
+++ proyectoUnoIPC/src/main/java/backen/serverts/inicioDeSesionSvt.java
@@ -4,7 +4,9 @@
  */
 package backen.serverts;
 
+import backen.DataBase.conexionDB;
 import backen.usuarios.Usuario;
+import enums.tipoUser;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
@@ -13,6 +15,10 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.mindrot.jbcrypt.BCrypt;
 
 /**
  *
@@ -21,13 +27,10 @@ import jakarta.servlet.http.HttpSession;
 @WebServlet(name = "inicioDeSesion", urlPatterns = {"/inicioDeSesion"})
 public class inicioDeSesionSvt extends HttpServlet {
 
-
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-       
     }
-
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -39,22 +42,48 @@ public class inicioDeSesionSvt extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         
+        ResultSet resultSet = null;
+        PreparedStatement preparedStatement = null;
         processRequest(request, response);
         Usuario user = new Usuario();
         user.setUserName(request.getParameter("User"));
         user.setPassword(request.getParameter("Password"));
-        
+        String tipoUsuarioStr = "";
+
         if (user.iniciarSesion(user)) {
-             HttpSession session = request.getSession();
+            HttpSession session = request.getSession();
             session.setAttribute("userName", user.getUserName());
-            System.out.println("inicio sesion");
-            response.sendRedirect("Perfil.jsp");
+            conexionDB conexion = new conexionDB();
+            String query = "SELECT tipo_usuario FROM USUARIO WHERE BINARY user_name = ?";
+            try {
+                preparedStatement = conexion.getConnection().prepareStatement(query);
+                preparedStatement.setString(1, user.getUserName()); // Asegúrate de obtener el nombre de usuario de `user`
+                resultSet = preparedStatement.executeQuery();
+                if (resultSet.next()) {
+                    tipoUsuarioStr = resultSet.getString("tipo_usuario");
+                }
+                
+                if (tipoUsuarioStr.equals(tipoUser.ADMINISTRADOR.name())) {
+                    response.sendRedirect("vistaAdmin.jsp");
+                } else if (tipoUsuarioStr.equals(tipoUser.AUTOR.name())) {
+                    response.sendRedirect("vistaAutor.jsp");
+                } else if (tipoUsuarioStr.equals(tipoUser.LECTOR.name())) {
+                    response.sendRedirect("vistaLectores.jsp");
+                } else if (tipoUsuarioStr.equals(tipoUser.PUBLICISTA.name())) {
+                    response.sendRedirect("vistaPublicista.jsp");
+                }
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } finally {
+                conexion.cerrarConnection(conexion.getConnection());
+            }
             System.out.println(user.getTipo());
-        }else{
+        } else {
             response.sendRedirect("Login.jsp?error=true");
             System.out.println("no inicar sesion");
         }
-        
+
     }
 
     @Override
